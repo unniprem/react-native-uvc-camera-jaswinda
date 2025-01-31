60dbd0c708184265d3809faa06eee8d1887c8388
unnikrishnan c <unni.prem@gmail.com>
2025-01-31 23:47:29 +0530

    may work

--- android/src/main/java/com/uvccamera/UVCCameraViewModule.kt
+++ android/src/main/java/com/uvccamera/UVCCameraViewModule.kt
@@ -5,11 +5,13 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.uimanager.UIManagerHelper
 import com.uvccamera.utils.withPromise
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class UVCCameraViewModule(reactContext: ReactApplicationContext?) :
   ReactContextBaseJavaModule(reactContext) {
@@ -17,8 +19,7 @@ class UVCCameraViewModule(reactContext: ReactApplicationContext?) :
 
   override fun getName() = TAG
 
-
-  private fun findCameraView(viewId: Int): UVCCameraView {
+  private suspend fun findCameraView(viewId: Int): UVCCameraView = withContext(Dispatchers.Main) {
     Log.d(TAG, "Finding UVCCameraView with id: $viewId")
     
     val uiManager = UIManagerHelper.getUIManager(reactApplicationContext, viewId)
@@ -34,42 +35,70 @@ class UVCCameraViewModule(reactContext: ReactApplicationContext?) :
       null
     }
 
-    return view ?: throw ViewNotFoundError(viewId).also {
+    return@withContext view ?: throw ViewNotFoundError(viewId).also {
       Log.e(TAG, "View with id $viewId is not a UVCCameraView")
     }
   }
 
-
   @ReactMethod
   fun openCamera(viewTag: Int) {
-    val view = findCameraView(viewTag)
-    view.openCamera()
+    coroutineScope.launch {
+      try {
+        val view = findCameraView(viewTag)
+        view.openCamera()
+      } catch (e: Exception) {
+        Log.e(TAG, "Error opening camera: ${e.message}")
+      }
+    }
   }
 
   @ReactMethod
   fun closeCamera(viewTag: Int) {
-    val view = findCameraView(viewTag)
-    view.closeCamera()
+    coroutineScope.launch {
+      try {
+        val view = findCameraView(viewTag)
+        view.closeCamera()
+      } catch (e: Exception) {
+        Log.e(TAG, "Error closing camera: ${e.message}")
+      }
+    }
   }
 
   @ReactMethod
   fun updateAspectRatio(viewTag: Int, width: Int, height: Int) {
-    val view = findCameraView(viewTag)
-    view.updateAspectRatio(width, height)
+    coroutineScope.launch {
+      try {
+        val view = findCameraView(viewTag)
+        view.updateAspectRatio(width, height)
+      } catch (e: Exception) {
+        Log.e(TAG, "Error updating aspect ratio: ${e.message}")
+      }
+    }
   }
 
   @ReactMethod
   fun setCameraBright(viewTag: Int, brightness: Int) {
-    val view = findCameraView(viewTag)
-    view.setCameraBright(brightness)
+    coroutineScope.launch {
+      try {
+        val view = findCameraView(viewTag)
+        view.setCameraBright(brightness)
+      } catch (e: Exception) {
+        Log.e(TAG, "Error setting brightness: ${e.message}")
+      }
+    }
   }
 
   @ReactMethod
   fun setZoom(viewTag: Int, zoom: Int) {
-    val view = findCameraView(viewTag)
-    view.setZoom(zoom)
+    coroutineScope.launch {
+      try {
+        val view = findCameraView(viewTag)
+        view.setZoom(zoom)
+      } catch (e: Exception) {
+        Log.e(TAG, "Error setting zoom: ${e.message}")
+      }
+    }
   }
-
 
   @ReactMethod
   fun takePhoto(viewTag: Int, promise: Promise) {
@@ -83,7 +112,17 @@ class UVCCameraViewModule(reactContext: ReactApplicationContext?) :
 
   @ReactMethod
   fun setDefaultCameraVendorId(viewTag: Int, vendorId: Int) {
-    val view = findCameraView(viewTag)
-    view.setDefaultCameraVendorId(vendorId)
+    coroutineScope.launch {
+      try {
+        val view = findCameraView(viewTag)
+        view.setDefaultCameraVendorId(vendorId)
+      } catch (e: Exception) {
+        Log.e(TAG, "Error setting default vendor ID: ${e.message}")
+      }
+    }
+  }
+
+  companion object {
+    private const val TAG = "UVCCameraViewModule"
   }
 }
